5e5c50c9b1b2fa067af5b9b7bcffd8918b32ed59
mma narola <mmanarola@outlook.com>
2021-08-20 21:16:22 +0530

    Splash activity created...

--- app/src/main/java/com/example/stepcounterapp/MainActivity.java
+++ app/src/main/java/com/example/stepcounterapp/MainActivity.java
@@ -2,14 +2,49 @@ package com.example.stepcounterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView logo,image;
+    TextView appname;
+    Button btnnext;
+    LottieAnimationView lottieAnimationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = findViewById(R.id.logo);
+        appname = findViewById(R.id.appname);
+        image = findViewById(R.id.image);
+        lottieAnimationView = findViewById(R.id.lottie);
+        btnnext = findViewById(R.id.btnnext);
+
+
+
+        image.animate().translationY(-1600).setDuration(1000).setStartDelay(3000);
+        btnnext.animate().translationY(-1600).setDuration(1000).setStartDelay(3000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(3000);
+        appname.animate().translationY(1400).setDuration(1000).setStartDelay(3000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(3000);
+
+        btnnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
 
