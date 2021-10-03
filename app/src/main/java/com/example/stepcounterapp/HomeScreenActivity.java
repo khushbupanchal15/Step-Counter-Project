f4e4d71cce6663f25204838750ad2c8ec6975caa
muni9989 <munira9918@gmail.com>
2021-10-03 14:13:05 +0530

    Modifications

--- app/src/main/java/com/example/stepcounterapp/HomeScreenActivity.java
+++ app/src/main/java/com/example/stepcounterapp/HomeScreenActivity.java
@@ -25,9 +25,8 @@ public class HomeScreenActivity extends AppCompatActivity {
         bottomNav = findViewById(R.id.bottomNav);
 
         bottomNav.add(new MeowBottomNavigation.Model(1,R.drawable.ic_home));
-        bottomNav.add(new MeowBottomNavigation.Model(2,R.drawable.ic_report));
-        bottomNav.add(new MeowBottomNavigation.Model(3,R.drawable.ic_health));
-        bottomNav.add(new MeowBottomNavigation.Model(4,R.drawable.ic_settings));
+        bottomNav.add(new MeowBottomNavigation.Model(2,R.drawable.ic_health));
+        bottomNav.add(new MeowBottomNavigation.Model(3,R.drawable.ic_settings));
 
         bottomNav.show(1,true);
         loadFragment(new HomeFragment());
@@ -41,15 +40,12 @@ public class HomeScreenActivity extends AppCompatActivity {
                         loadFragment(new HomeFragment());
                         break;
                     }
+
                     case 2: {
-                        loadFragment(new ReportFragment());
-                        break;
-                    }
-                    case 3: {
                         loadFragment(new HealthFragment());
                         break;
                     }
-                    case 4:{
+                    case 3:{
                         loadFragment(new SettingsFragment());
                         break;
                     }
